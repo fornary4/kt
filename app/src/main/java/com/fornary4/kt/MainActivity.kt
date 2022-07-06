717a2cfee245742b82acf82431339181f126f1da
fornary4 <2839395512@qq.com>
2022-07-06 11:58:06 +0800

    privacy

--- app/src/main/java/com/fornary4/kt/MainActivity.kt
+++ app/src/main/java/com/fornary4/kt/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.fornary4.kt
 
-import android.content.BroadcastReceiver
-import android.content.Context
+import android.Manifest
+
 import android.content.Intent
-import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
-import java.io.*
-import java.lang.StringBuilder
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,23 +21,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btn_submit).setOnClickListener {
-            val editor = getSharedPreferences("data", Context.MODE_PRIVATE).edit()
-            editor.putString("name", "Tom")
-            editor.putInt("age", 20)
-            editor.putBoolean("married", false)
-            editor.apply()
+        findViewById<Button>(R.id.btn_call).setOnClickListener {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE), 1)
+            } else {
+                call()
+            }
         }
+    }
 
-        findViewById<Button>(R.id.btn_read).setOnClickListener {
-            val pref = getSharedPreferences("data", Context.MODE_PRIVATE)
-            val name = pref.getString("name", "")
-            val age = pref.getInt("age", 0)
-            val married = pref.getBoolean("married", false)
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    call()
+                } else {
+                    Toast.makeText(this, "You denied the permission", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
 
-            Log.d("forntag", "name is $name")
-            Log.d("forntag", "age is $age")
-            Log.d("forntag", "married is $married")
+
+    private fun call() {
+        try {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:10086")
+            startActivity(intent)
+        } catch (e: SecurityException) {
+            e.printStackTrace()
         }
     }
 
