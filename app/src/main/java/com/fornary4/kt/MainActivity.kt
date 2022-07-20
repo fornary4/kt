de7ec26e842757063223c61166429f5b782c0516
fornary4 <2839395512@qq.com>
2022-07-20 11:11:34 +0800

    viewBinding

--- app/src/main/java/com/fornary4/kt/MainActivity.kt
+++ app/src/main/java/com/fornary4/kt/MainActivity.kt
@@ -20,39 +20,7 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val userDao = AppDatabase.getDatabase(this).userDao()
-        val user1 = User("Tom", "Brady", 40)
-        val user2 = User("Tom", "Hanks", 63)
 
-
-
-        binding.addDataBtn.setOnClickListener {
-            thread {
-                user1.id = userDao.insertUser(user1)
-                user2.id = userDao.insertUser(user2)
-            }
-        }
-
-        binding.updateDataBtn.setOnClickListener {
-            thread {
-                user1.age = 42
-                userDao.updateUser(user1)
-            }
-        }
-
-        binding.deleteDataBtn.setOnClickListener {
-            thread {
-                userDao.deleteUserByLastname("Hanks")
-            }
-        }
-
-        binding.queryDataBtn.setOnClickListener {
-            thread {
-                for (user in userDao.loadAllUsers()) {
-                    Log.d("forntag", user.toString())
-                }
-            }
-        }
         
 
     }
